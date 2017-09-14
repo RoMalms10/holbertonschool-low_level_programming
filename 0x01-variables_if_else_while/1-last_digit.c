c0270d13ae0d77402d027db929317eaf516766ff
RoMalms10 <romalms10@gmail.com>
2017-09-14 22:59:11 +0000

    Create a program that uses modular arithmetic

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry Point
+  * This program will say what the last numberof n is, and compare it
+  * against 5, 6 and 0.
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = (n % 10);
+	if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	else
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	return (0);
+}
