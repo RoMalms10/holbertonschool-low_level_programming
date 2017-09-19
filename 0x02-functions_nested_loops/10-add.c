d51be46dbe8e67b9b522f1cfd5c34ad57e96e87b
RoMalms10 <156@holbertonschool.com>
2017-09-19 19:59:50 +0000

    Add comments

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
-  *
-  *
-  *
+  * add - adds two integers together
+  * @i: is passed the first integer to be added
+  * @j: is passed the second integer to be added
+  * Return: k, the value of the addition
   */
 
 int add(int i, int j)
 {
 	int k;
 
-	k = i +j;
-	return(k);
+	k = i + j;
+	return (k);
 }
