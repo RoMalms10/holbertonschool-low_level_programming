ef5d5712fa238d41268346ada6d450290c6326ef
RoMalms10 <156@holbertonschool.com>
2017-09-20 04:09:33 +0000

    Create function to print first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  *
+  *
+  *
+  *
+  */
+
+int main(void)
+{
+	long int a, b, i, sum;
+
+	a = 1;
+	b = 2;
+	for (i = 3; i < 50; i++)
+	{
+		sum = a + b;
+		if (i == 49)
+		{
+			printf("%ld\n", sum);
+			break;
+		}
+		printf("%ld, ", sum);
+		a = b;
+		b = sum;
+	}
+	return (0);
+}
