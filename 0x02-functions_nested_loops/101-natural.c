d59263a67b898af436b1f5867ae4f34532840ade
RoMalms10 <156@holbertonschool.com>
2017-09-20 01:57:21 +0000

    Update file to split up variable

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
   * main - Entry Point
@@ -9,9 +10,10 @@
 int main(void)
 {
 	int i;
-	int sum = 0;
+	int sum;
 
-	for (i = 1; i < 1024; i++)
+	sum = 0;
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = i + sum;
