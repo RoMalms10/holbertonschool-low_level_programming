6e81ac546b9ac5828ba4f59f5a1a82b757487c39
RoMalms10 <156@holbertonschool.com>
2017-09-21 21:56:03 +0000

    Create a function to replace numbers in 1 to 100

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  * This Program will print out 1 to 100 and replace certain numbers
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
