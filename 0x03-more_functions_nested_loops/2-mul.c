e080ca78068f376eb6c3306f696717f8050471ca
RoMalms10 <156@holbertonschool.com>
2017-09-21 18:00:55 +0000

    Create function that multiplies two numbers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * mul - Takes two integers and multiplies them
+  * @a: the first number taken to be multiplied
+  * @b: the second number taken to be multiplied
+  * Return: the value of the multiplication
+  */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
