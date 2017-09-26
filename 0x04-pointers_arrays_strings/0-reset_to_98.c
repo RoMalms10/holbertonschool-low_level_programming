43126a10c0a3a1085cc738ccf0242282745bee43
RoMalms10 <156@holbertonschool.com>
2017-09-26 03:52:37 +0000

    Create function to reassign value using pointer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+  * reset_to_98 - Will reassign the memory location passed to the function
+  * @n: Accepts the memory location and then modifies the contents
+  * Return: Nothing, void
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
