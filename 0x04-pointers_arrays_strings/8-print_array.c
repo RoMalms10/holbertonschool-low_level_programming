5e0b7bad5f33f54eef06b14642b1c762357241f0
RoMalms10 <156@holbertonschool.com>
2017-09-26 20:40:08 +0000

    Fix formatting error for new line

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,13 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	n = n - 1;
 	x = 0;
 	while (x < n)
 	{
-		printf("%d, ", a[x]);
+		printf("%d", a[x]);
+		if (x != n - 1)
+			printf(", ");
 		x++;
 	}
-	if (n >= 0)
-		printf("%d\n", a[x]);
+	printf("\n");
 }
