da6ba5b677e7116c536ce92344201fa44ed70cef
RoMalms10 <156@holbertonschool.com>
2017-09-26 18:07:48 +0000

    Create function to print elements of arrays

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * print_array - Prints n elements of an array in order based on number passed
+  * @a: the array passed
+  * @n: specifies how many elements to print
+  * Return: Nothing, void
+  */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	n = n - 1;
+	x = 0;
+	while (x < n)
+	{
+		printf("%d, ", a[x]);
+		x++;
+	}
+	printf("%d\n", a[x]);
+}
