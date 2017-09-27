91c204c7bc9f6799dc05f484e30b47e11cbce094
RoMalms10 <156@holbertonschool.com>
2017-09-27 01:08:40 +0000

    Update condition for no number

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 
 /**
-  *
-  *
-  *
-  *
+  * _atoi - Takes a string and converts the numbers into integers
+  * @s: The string to be checked
+  * Return: Nothing, void
   */
 
 int _atoi(char *s)
@@ -13,7 +12,7 @@ int _atoi(char *s)
 	unsigned int final;
 
 	x = negative = sign = number = final = 0;
-	while (!(s[x] >= 48 && s[x] <= 57))
+	while (!(s[x] >= 48 && s[x] <= 57)) /*while NOT a number*/
 	{
 		if (s[x] == '-')
 			negative++;
@@ -23,20 +22,22 @@ int _atoi(char *s)
 		sign = -1;
 	hold = x;
 	counter = 1;
-	while (s[x] >= 48 && s[x] <= 57)
+	while (s[x] >= 48 && s[x] <= 57) /*while it IS a number*/
 	{
 		counter *= 10;
 		x++;
 	}
 	counter /= 10;
-	while (s[hold] >= 48 && s[hold] <= 57)
+	while (s[hold] >= 48 && s[hold] <= 57) /*where x found first number*/
 	{
 		number = (s[hold] - '0') * counter;
 		final = final + number;
 		counter /= 10;
-		hold ++;
+		hold++;
 	}
 	if (sign < 0)
 		final = final * -1;
+	if (final == 0)
+		return (0);
 	return (final);
 }
