1c2ebd55fe21cae2a85602fb06319f3f8cf3c94d
RoMalms10 <156@holbertonschool.com>
2017-09-27 01:50:36 +0000

    Update function to be compact

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,36 +8,23 @@
 
 int _atoi(char *s)
 {
-	int x, hold, counter, negative, sign, number;
-	unsigned int final;
+	int x, sign;
+	unsigned int number;
 
-	x = negative = sign = number = final = 0;
+	x = number = 0;
+	sign = 1;
 	while (!(s[x] >= 48 && s[x] <= 57)) /*while NOT a number*/
 	{
 		if (s[x] == '-')
-			negative++;
+			sign *= -1;
 		x++;
 	}
-	if (negative % 2 != 0)
-		sign = -1;
-	hold = x;
-	counter = 1;
-	while (s[x] >= 48 && s[x] <= 57) /*while it IS a number*/
+	if (s[x] == '\0')
+		return (number);
+	while (s[x] >= 48 && s[x] <= 57) /*While it IS a number*/
 	{
-		counter *= 10;
+		number = number * 10 + (s[x] - '0');
 		x++;
 	}
-	counter /= 10;
-	while (s[hold] >= 48 && s[hold] <= 57) /*where x found first number*/
-	{
-		number = (s[hold] - '0') * counter;
-		final = final + number;
-		counter /= 10;
-		hold++;
-	}
-	if (sign < 0)
-		final = final * -1;
-	if (final == 0)
-		return (0);
-	return (final);
+	return (number * sign);
 }
