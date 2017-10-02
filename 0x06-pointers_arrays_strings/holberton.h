4ce9c1cc936ee6895acfa2559a7e474d74073a56
RoMalms10 <156@holbertonschool.com>
2017-10-02 20:22:57 +0000

    Edit #define

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HHOLBERTON_H_SEEN
-#define
+#ifndef HOLBERTON_H_SEEN
+#define HOLBERTON_H_SEEN
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
