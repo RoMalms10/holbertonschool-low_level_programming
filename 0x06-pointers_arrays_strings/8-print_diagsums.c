9d9d8e5894c85362ec62a1bf8be54b0420ac63d7
RoMalms10 <156@holbertonschool.com>
2017-10-03 03:29:34 +0000

    add descriptions

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -12,24 +12,24 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum, max;
 
-	max = size * size;
+	max = size * size; /*size of array - size by size square*/
 	i = sum = 0;
 	while (i < max)
 	{
 		sum = sum + a[i];
-		if (i == (max - 1))
+		if (i == (max - 1)) /*reached end of diagonal - bottom right*/
 		{
 			printf("%d, ", sum);
 			break;
 		}
-		i = i + (size + 1);
+		i = i + (size + 1); /*add after because you start top left*/
 	}
 	sum = 0;
 	while (i >= (size - 1))
 	{
-		i = i - (size - 1);
+		i = i - (size - 1); /*subtract first to get to bottom left*/
 		sum = sum + a[i];
-		if (i == (size - 1))
+		if (i == (size - 1)) /*reached end of diagonal - top right*/
 		{
 			printf("%d\n", sum);
 			break;
