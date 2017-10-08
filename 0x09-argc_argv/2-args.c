02cbf3a219a7a26e35e05dbc9c3fd32cc039685f
RoMalms10 <156@holbertonschool.com>
2017-10-08 03:36:18 +0000

    Create a function to print arguments

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  * @argc: The number of arguments passed
+  * @argv: An array of pointers to arguments
+  * Return: 0, always success
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
