31b725edef8abfb3ef2d91432836d22a33bc3bc6
RoMalms10 <156@holbertonschool.com>
2017-10-11 01:56:20 +0000

    Create function to free allocated space

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * free_grid - frees memory allocated to the grid array
+  * @grid: the array to free
+  * @height: the amount of row to free
+  * Return: Nothing, void
+  */
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL || height <= 0)
+		return;
+	for (; height > 0; height--)
+		free(grid[height]);
+	free(grid);
+}
