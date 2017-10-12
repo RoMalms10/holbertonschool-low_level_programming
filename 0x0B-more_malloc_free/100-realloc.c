6b6de2c6323b3236065773c5c1164849217f4c3e
RoMalms10 <156@holbertonschool.com>
2017-10-12 21:30:40 +0000

    Create function to recreate realloc

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _realloc - Creates newly allocated space of size new_size and copies
+  * original contents there and leaves new memory uninitialized
+  * @ptr: the pointer to the old allocated space of memory
+  * @old_size: The old size of the old allocated space
+  * @new_size: The new size wanted in the newly allocated space
+  * Return: pointer to the newly allocated space in memory
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *hold;
+	unsigned int i;
+
+	hold = (char *)ptr;
+	if (new_size == old_size)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size < old_size)
+		old_size = new_size;
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= old_size; i++)
+		p[i] = hold[i];
+	free(ptr);
+	return (p);
+}
