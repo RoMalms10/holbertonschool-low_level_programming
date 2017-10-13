0a2c852e5adb986881db94f1cbba5a0fd775afbf
RoMalms10 <156@holbertonschool.com>
2017-10-13 19:12:01 +0000

    Update for loop to not overflow

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (ptr != NULL)
 	{
-		for (i = 0; i <= old_size; i++)
+		for (i = 0; i < old_size; i++)
 			p[i] = hold[i];
 	}
 	free(ptr);
