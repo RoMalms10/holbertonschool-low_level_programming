6c64b871e27758ff50edc01c4ffdb9618e157d8e
RoMalms10 <156@holbertonschool.com>
2017-10-17 01:31:02 +0000

    Malloc space for strings

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 
 /**
+  * _strdup - duplicates a string
+  * @str: the string to duplicate
+  * Return: NULL if malloc fails or if str is NULL, else pointer to new string
+  */
+
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, count;
+
+	count = 0;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	ptr = malloc(sizeof(char) * (count + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < count; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+
+/**
   * new_dog - Creates a new dog out of passed parameters
   * @name: Name of the dog
   * @age: Age of the dog
@@ -11,13 +38,17 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *name_hold;
+	char *owner_hold;
 	dog_t *Luna;
 
+	name_hold = _strdup(name);
+	owner_hold = _strdup(owner);
 	Luna = malloc(sizeof(dog_t));
 	if (Luna == NULL)
 		return (NULL);
-	Luna->name = name;
+	Luna->name = name_hold;
 	Luna->age = age;
-	Luna->owner = owner;
+	Luna->owner = owner_hold;
 	return (Luna);
 }
