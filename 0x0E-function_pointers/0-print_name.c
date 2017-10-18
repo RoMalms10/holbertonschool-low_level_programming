6f4ab737509c1a7aa6d8d607a1b6eecf54b0df03
RoMalms10 <156@holbertonschool.com>
2017-10-18 21:03:14 +0000

    Add function description

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+  * print_name - prints a name based on what function is passed to it
+  * @name: the name to print
+  * @f: the function being passed, which is the format to print name
+  * Return: nothing, void
+  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
