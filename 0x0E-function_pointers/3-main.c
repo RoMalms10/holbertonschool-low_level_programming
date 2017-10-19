00b438d8130d94c5c91fdfab638f63939fa2fe7e
RoMalms10 <156@holbertonschool.com>
2017-10-19 04:04:18 +0000

    Create program that chooses functions off of argv

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+  * main - Entry Point
+  * @argc: Number of arguments passed to function
+  * @argv: Array of pointers to strings(arguments)
+  * Return: 0 if success, 98 - 100 for different errors
+  */
+
+int main(int argc, char *argv[])
+{
+	int (*funcptr)(int, int); /*Need to hold the address later*/
+	int capture; /*Used to hold the value after function executes*/
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	ag1 = atoi(argv[1]);
+	ag3 = atoi(argv[3]);/*Have to check here to see if atoi fails (0)*/
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && ag3 == '0')
+		/*Checks if the first char in the string is / or %*/
+	{
+		printf("Error\n");
+		return (100);
+	}
+	funcptr = get_op_func(argv[2]);
+	if (funcptr == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	capture = funcptr(ag1, ag3);
+	printf("%d\n", capture);
+	return (0);
+}
