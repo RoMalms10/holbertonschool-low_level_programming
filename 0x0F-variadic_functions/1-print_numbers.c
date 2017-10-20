1a3dda1cb42b9e68d41cbeea6304181c3b30536f
RoMalms10 <156@holbertonschool.com>
2017-10-20 21:34:24 +0000

    Create program to print numbers passed

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - prints the numbers passed as arguments
+  * @separator: string to print in between numbers
+  * @n: the amount of numbers passed
+  * Return: Nothing, void
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list valist;
+
+	if (separator == NULL)
+		return;
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf("%s", separator);
+		printf("%d", va_arg(valist, int));
+	}
+	printf("\n");
+	va_end(valist);
+}
