0f2900071a58fc3a101b1d447d2da43f9c55a044
RoMalms10 <156@holbertonschool.com>
2017-10-21 20:26:31 +0000

    Update edge cases

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,12 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list valist;
 
-	if (separator == NULL)
+	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
+		if (i != 0 && separator != NULL)
 			printf("%s", separator);
 		printf("%d", va_arg(valist, int));
 	}
