b3e6f685922737718056ce51c25a0cfe63099dc7
RoMalms10 <156@holbertonschool.com>
2017-10-31 01:26:02 +0000

    Create program to free list and set head to NULL

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a single linked list and sets the head back to NULL
+  * @head: double pointer to the beginning of the list
+  * Return: nothing, void
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
