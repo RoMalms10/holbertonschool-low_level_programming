638ada6e9acef49ba092c0f3e443c8dc90d55464
RoMalms10 <156@holbertonschool.com>
2017-11-03 01:46:32 +0000

    Update return description

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
   * get_bit - checks if a bit at the index is a 1 or a 0
   * @n: the number to check
   * @index: the spot in the number n to check
-  * Return: a 1 if the number at the index is a 1, 0 otherwise
+  * Return: a 1 if the number at the index is a 1, -1 otherwise
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
