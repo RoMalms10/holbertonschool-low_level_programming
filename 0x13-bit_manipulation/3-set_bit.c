4cbe360dc0692698e8f9ab5a472602e1e76dd7ea
RoMalms10 <156@holbertonschool.com>
2017-11-04 01:32:24 +0000

    Update max index length

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (n == NULL)
 		return (-1);
-	if ((sizeof(unsigned long int) * 8) < index)
+	if ((sizeof(unsigned long int) * 8) <= index)
 		return (-1);
 	set = 1;
 	set = set << index;
