e4e7c786eba47a9846e7f218470ab97f948c1a8f
RoMalms10 <156@holbertonschool.com>
2017-11-04 01:32:46 +0000

    Update max index length

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (n == NULL)
 		return (-1);
-	if ((sizeof(unsigned long int) * 8) < index)
+	if ((sizeof(unsigned long int) * 8) <= index)
 		return (-1);
 	mask = 1;
 	mask = mask << index;
