0d993208824dfb46dbf0561ce475979de904a511
RoMalms10 <156@holbertonschool.com>
2017-11-08 16:49:51 +0000

    Edit BUF_SIZE

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <stdarg.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 1204
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
