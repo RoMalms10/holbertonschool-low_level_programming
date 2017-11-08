7bb62ba288448926c38f5b0141cfc8874c14fd25
RoMalms10 <156@holbertonschool.com>
2017-11-08 16:51:10 +0000

    Revert BUF_SIZE

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <stdarg.h>
 
-#define BUF_SIZE 1204
+#define BUF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
