f368ac3818b96cd26efef012eb927215fd530db8
RoMalms10 <156@holbertonschool.com>
2017-11-08 19:10:47 +0000

    Initialize i before loop

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -16,10 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (opened == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
 		i = 0;
+	}
 	else
+	{
+		i = 0;
 		while (text_content[i] != '\0')
 			i++;
+	}
 	wrote = write(opened, text_content, i);
 	if (wrote == -1)
 		return (-1);
