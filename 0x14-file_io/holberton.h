9f18fc3b7580f6de53226e483fdd8aa11de3c539
RoMalms10 <156@holbertonschool.com>
2017-11-08 05:28:08 +0000

    Add function declarations for file 3

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,9 +5,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <stdarg.h>
+
+#define BUF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int writing_file(int file_from, int file_to);
+void print_error(int error_number, ...);
 
 #endif
