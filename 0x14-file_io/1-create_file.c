daae6b84284f4ec1186677fece246c3cbf4899b0
RoMalms10 <156@holbertonschool.com>
2017-11-08 19:09:41 +0000

    Initialize i before loop

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,10 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (opened == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
 		i = 0;
+	}
 	else
+	{
+		i = 0;
 		while (text_content[i] != '\0')
 			i++;
+	}
 	wrote = write(opened, text_content, i);
 	if (wrote == -1)
 		return (-1);
