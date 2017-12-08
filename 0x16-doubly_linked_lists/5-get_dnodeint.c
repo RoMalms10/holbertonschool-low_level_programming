529cc9a2677a8d569ec5328fc88c35e41fbfb824
RoMalms10 <156@holbertonschool.com>
2017-12-08 21:39:49 +0000

    Create C program that returns a node's information

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - goes to specified node and returns it
+  * @head: pointer to the list
+  * @index: the node to return
+  * Return: the node specified, NULL if no list
+  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter;
+
+	if (head == NULL)
+		return (NULL);
+	for (counter = 0; counter < index; counter++)
+		head = head->next;
+	return (head);
+}
