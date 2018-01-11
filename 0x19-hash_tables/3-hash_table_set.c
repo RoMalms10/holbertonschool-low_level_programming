55d949af36c59f099507a61d7d7aac2118981f2e
RoMalms10 <156@holbertonschool.com>
2018-01-11 23:51:34 +0000

    Create a C function that adds to the array

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - Inserts a key and value in the array
+  * @ht: the hash table
+  * @key: the key to put in the array
+  * @value: the value ot assign to they key
+  * Return: 1 if succeeded, 0 if not
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index = 0;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
