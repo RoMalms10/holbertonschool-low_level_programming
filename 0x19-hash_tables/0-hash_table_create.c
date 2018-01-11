6d79f03a10c79f2632af3d218c08e62876b52598
RoMalms10 <156@holbertonschool.com>
2018-01-11 23:45:20 +0000

    Initialize each element of array to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
@@ -19,6 +20,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(ht);
 		return (NULL);
+	}
+
+	/*Assign each element of array to NULL*/
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
 	}
 
 	return (ht);
