73ac254a6efb073f2bc5133a5f074d19d357691f
RoMalms10 <156@holbertonschool.com>
2018-01-12 22:11:03 +0000

    Add fail checks

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *search = NULL;
 	unsigned long int index = 0;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 
