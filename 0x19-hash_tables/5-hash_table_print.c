1773a2574d9da3b9762172819061b5a5859ff34f
RoMalms10 <156@holbertonschool.com>
2018-01-12 01:39:33 +0000

    Create function that prinst hash table

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key,
+						ht->array[i]->value);
+			if (ht->array[i]->next != NULL)
+				print_list(ht->array[i]);
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
+
+void print_list(hash_node_t *list)
+{
+	while (list != NULL)
+	{
+		printf(", ");
+		printf("'%s': '%s'", list->key, list->value);
+		list = list->next;
+	}
+}
