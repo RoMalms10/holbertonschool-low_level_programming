83066ebc336c746f27ab8bd397414f351c4233dd
RoMalms10 <156@holbertonschool.com>
2018-01-12 01:43:56 +0000

    Add movement through a list in while loop

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -30,6 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			search->value = (char *)value;
 			return (1);
 		}
+		search = search->next;
 	}
 
 	new_node->key = (char *)key;
