a945110c07f1b0d6a2ad443761149979d97c0f7c
RoMalms10 <156@holbertonschool.com>
2018-01-26 23:28:21 +0000

    Update insertion method to slide nodes through list

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,11 +1,9 @@
 #include "sort.h"
 
 /**
-  *
-  *
-  *
-  *
-  *
+  * insertion_sort_list - insertion sort for doubly linked list
+  * @list: the doubly link list to sort using insertion sort method
+  * Return: nothing, void
   */
 void insertion_sort_list(listint_t **list)
 {
@@ -23,28 +21,29 @@ void insertion_sort_list(listint_t **list)
 		if (sorted->n > compare->n)
 		{
 			/*find where the node goes in the sorted part*/
-			while (sorted->prev != NULL)
+			while (sorted != NULL)
 			{
-				if (sorted->prev->n < compare->n)
+				if (sorted->n < compare->n)
 					break;
-				sorted = sorted->prev;
+				/*Remove node from list*/
+				compare->prev->next = compare->next;
+				if (compare->next != NULL)
+					compare->next->prev = compare->prev;
+				/*move node above sorted pointer*/
+				if (sorted->prev != NULL)
+					sorted->prev->next = compare;
+				compare->prev = sorted->prev;
+				/*connect the compared node to the sorted node*/
+				compare->next = sorted;
+				sorted->prev = compare;
+				/*move sorted up one before compare*/
+				sorted = compare->prev;
+				/*move the head of the list if needed*/
+				if (compare->prev == NULL)
+					*list = compare;
+				print_list(*list);
 			}
-			/*Remove node from list*/
-			compare->prev->next = after;
-			if (after != NULL)
-				after->prev = compare->prev;
-			/*move node above sorted pointer*/
-			if (sorted->prev != NULL)
-				sorted->prev->next = compare;
-			compare->prev = sorted->prev;
-			/*connect the compared node to the sorted node*/
-			compare->next = sorted;
-			sorted->prev = compare;
-			/*move the head of the list if needed*/
-			if (compare->prev == NULL)
-				*list = compare;
-			print_list(*list);
 		}
-		compare = compare->next;
+		compare = after;
 	}
 }
