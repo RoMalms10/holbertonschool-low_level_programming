e7812e45acb5d9f6a1704bb916c3142b892ad09e
RoMalms10 <156@holbertonschool.com>
2018-01-29 03:39:28 +0000

    Create lomuto partition quick sort

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,64 @@
+#include "sort.h"
+
+/**
+  * quick_sort - implements quick sort on an array
+  * @array: the array to sort
+  * @size: the size of the array
+  * Return: nothing, void
+  */
+void quick_sort(int *array, size_t size)
+{
+	lomuto_sort(array, 0, (size - 2), size);
+}
+
+/**
+  * lomuto_sort - sorts an array using quick sort lomuto partition
+  * @array: the array to sort
+  * @LP: the left pointer
+  * @RP: the right pointer
+  * @size: the size of the array
+  * Return: nothing, void
+  */
+void lomuto_sort(int *array, int LP, int RP, size_t size)
+{
+	int Pivot = RP + 1;
+	int temp;
+	int L_moved = 0;
+
+	if (RP < LP || RP == LP)
+		return;
+	while (RP > LP && RP != LP)
+	{
+		if (array[LP] > array[Pivot])
+		{
+			for (; LP < RP && LP != RP; RP--)
+			{
+				if (array[RP] < array[Pivot])
+				{
+					temp = array[LP];
+					array[LP] = array[RP];
+					array[RP] = temp;
+					LP++;
+					L_moved++;
+					RP--;
+					print_array(array, size);
+					break;
+				}
+			}
+		}
+		else
+		{
+			LP++;
+			L_moved++;
+		}
+	}
+	if (array[LP] > array[Pivot])
+	{
+		temp = array[Pivot];
+		array[Pivot] = array[LP];
+		array[LP] = temp;
+		print_array(array, size);
+	}
+	lomuto_sort(array, (LP - L_moved), (RP - 1), size); /*Left side recursion*/
+	lomuto_sort(array, (LP + 1), (Pivot - 1), size); /*Right side recursion*/
+}
