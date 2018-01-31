feb7d8ffcc3bd2485799b61ea59191a65ee1eefd
RoMalms10 <156@holbertonschool.com>
2018-01-31 06:33:11 +0000

    Create shell sort

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+  * shell_sort - sorts an array using shell implementation
+  * @array: the array to sort
+  * @size: the size of the array
+  * Return: nothing, void
+  */
+void shell_sort(int *array, size_t size)
+{
+	int temp;
+	int gap;
+	int index;
+	int index_2;
+	int arr_size = size;
+
+	gap = 1;
+	while (gap < arr_size)
+		gap = ((gap * 3) + 1);
+	gap = ((gap - 1) / 3); /*Decrement once because 1 iteration over now*/
+
+	while (gap > 0)
+	{
+		index = 0;
+		while ((index + gap) < arr_size)
+		{
+			if (array[index] > array[index + gap])
+			{
+				temp = array[index];
+				array[index] = array[index + gap];
+				array[index + gap] = temp;
+				index_2 = index;
+				while ((index_2 - gap) > 0 && array[index_2]
+						< array[index_2 - gap])
+				{
+					temp = array[index_2];
+					array[index_2] = array[index_2 - gap];
+					array[index_2 - gap] = temp;
+					index_2--;
+				}
+			}
+			index++;
+		}
+		print_array(array, size);
+		gap = ((gap - 1) / 3);
+	}
+}
