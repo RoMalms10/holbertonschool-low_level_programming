72811a51cade74f4f832a643cc597bc28734daa4
RoMalms10 <156@holbertonschool.com>
2018-02-16 23:09:21 +0000

    Calculate island perimeter

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" My module for calculating island perimeter """
+
+
+def island_perimeter(grid):
+    """ Takes a grid with an island in it and calculates the perimeter
+
+    Arguments:
+        grid (matrix): contains 1's and 0's to signify an island
+                       1 for land, 0 for water
+    """
+
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row - 1 >= 0 and grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row + 1 <= len(grid) - 1 and grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col - 1 >= 0 and grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col + 1 <= len(grid[row]) - 1 and grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
