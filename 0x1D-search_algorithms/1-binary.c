4199cdd73b0ba3a01c111ae6589f603f37d492ab
romalms10 <romalms10@gmail.com>
2018-04-12 21:55:01 +0000

    Implement Binary search on an array
    Use binary search on an array to find the value passed.
    Passed to a recursive function

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,26 +1,59 @@
 #include "search_algos.h"
 
 /**
-  *
-  *
-  *
-  *
-  *
-  *
+  * binary_search - finds the value passed in the array that's passed
+  * by sending off the values to a helper recursive function
+  * @array: the array to search through
+  * @size: the size of the array
+  * @value: the value looking for
+  * Return: the index of the value in the array, or -1 if none
   */
 int binary_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
 		return (-1);
-	return (binary_search_helper(array, 0, size, value));
+	return (binary_search_helper(array, 0, size - 1, value));
 
 
 }
 
-
+/**
+  * binary_search_helper - recursively checks the array until the
+  * value is found  by cutting the array in half each check
+  * @array: the array to seach
+  * @lo: the lowest point in the array to start at
+  * @hi: the highest point in the array to go to
+  * @value: the value looking for
+  * Return: the index of the value in the array, or -1 if none
+  */
 int binary_search_helper(int *array, size_t lo, size_t hi, int value)
 {
-	size_t mid = 0;
+	size_t mid;
+	size_t index;
 
+	if (lo <= hi)
+	{
+		/* Print the array to search through */
+		printf("Searching in array: ");
+		for (index = lo; index < hi; index++)
+		{
+			printf("%d", array[index]);
+			printf(", ");
+		}
+		printf("%d\n", array[index]);
 
+		/* Find midpoint */
+		mid = lo + ((hi - lo) / 2);
+
+		/* Check if midpoint is the value */
+		if (array[mid] == value)
+			return (mid);
+		/* Look at the right side of the array */
+		else if (array[mid] < value)
+			return (binary_search_helper(array, mid + 1, hi, value));
+		/* Look at the left side of the array */
+		else
+			return (binary_search_helper(array, 0, mid - 1, value));
+	}
+	return (-1);
 }
